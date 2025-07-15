de39b89d5708ee36954843dcfeeb24f7c219debf
thanusha gali <gali.thanusha1205@gmail.com>
2025-07-15 05:56:38 +0000

    fix: remove static export for server support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+ 
   basePath: process.env.NODE_ENV === 'production' ? '/DigitalWallet' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/DigitalWallet/' : '',
   typescript: {
